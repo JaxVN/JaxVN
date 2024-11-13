65fcbc6d99dd1adb539367713122c7baa0e1d432
JaxVN <hongbaongan@gmail.com>
2024-11-13 11:55:54 +0700

    Update docker.sh
    add confirm -y

--- script/docker.sh
+++ script/docker.sh
@@ -1,6 +1,6 @@
 # 1 line
 # 
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/JaxVN/jaxvn.github.io/refs/heads/main/script/docker.sh)" - y
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/JaxVN/jaxvn.github.io/refs/heads/main/script/docker.sh)"
 #
 # Add Docker's official GPG key:
 sudo apt-get update
@@ -11,5 +11,5 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
 $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
